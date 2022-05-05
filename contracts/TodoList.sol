4f8ac5ee005a808c865724de8a525b1e56437649
kjboogie <trickxbooks@gmail.com>
2022-05-05 22:14:42 +0530

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+//Contacts are crrated using "contract" keyword
+contract TodoList {
+
+ //State variable inside solidity are actually written to the blockchain.They actually represents state of the smart contract on the blockchain
+  uint public tasksCount = 0;
+
+}
